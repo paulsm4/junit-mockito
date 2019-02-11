c6155cafad1f53bd8141d2c56bcf1936e33129e8
PSantamaria <PSantamaria@pyramidsolutions.com>
2019-02-10 18:30:16 -0800

    Through Lesson 10: added Junit4 @Before/@After.  Next step: will migrate project to JUnit5.

--- HelloJUnit/test/com/example/junit/GreetingImplTest.java
+++ HelloJUnit/test/com/example/junit/GreetingImplTest.java
@@ -2,14 +2,31 @@ package com.example.junit;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class GreetingImplTest {
 
+	private Greeting greeting;
+
+	@Before
+	public void setup() {
+		System.out.println("setup()...");
+		greeting = new GreetingImpl();
+	}
+	
+	@After
+	public void teardown() {
+		System.out.println("teardown()...");
+		greeting = null;
+	}
+	
 	@Test
 	public void greetShouldReturnAValidValue() {
+		System.out.println("greetShouldReturnAValidValue()...");
 		//fail("Not yet implemented");    // This will fail
-		Greeting greeting = new GreetingImpl();
+		//Greeting greeting = new GreetingImpl();
 		String result = greeting.greet("junit");
 		assertNotNull(result);
 		assertEquals("Hello junit", result);
@@ -17,14 +34,16 @@ public class GreetingImplTest {
 
 	@Test(expected = IllegalArgumentException.class)
 	public void greetShouldThrowExceptionIfNameIsNull() {
-		Greeting greeting = new GreetingImpl();
+		System.out.println("greetShouldThrowExceptionIfNameIsNull()...");
+		//Greeting greeting = new GreetingImpl();
 		//String result = greeting.greet("junit");  // This will fail
 		String result = greeting.greet(null);
 	}
 
 	@Test(expected = IllegalArgumentException.class)
 	public void greetShouldThrowExceptionIfNameEmpty() {
-		Greeting greeting = new GreetingImpl();
+		System.out.println("greetShouldThrowExceptionIfNameEmpty()...");
+		//Greeting greeting = new GreetingImpl();
 		//String result = greeting.greet("junit");  // This will fail
 		String result = greeting.greet("");
 	}
