f0e267dbd24dea0b3c52c950a32830ff4f5818a1
PSantamaria <PSantamaria@pyramidsolutions.com>
2019-02-11 17:22:07 -0800

    Through Lesson 31: OrderBOImpl unit tests: "happy path", operation failure, exception

--- OrderProcessingService/src/test/java/com/example/order/bo/OrderBOImplTest.java
+++ OrderProcessingService/src/test/java/com/example/order/bo/OrderBOImplTest.java
@@ -41,29 +41,91 @@ import java.sql.SQLException;
  */
 public class OrderBOImplTest {
 
+	private OrderBOImpl bo;
+	private final int ID = 123;
+
 	// Stubbed out class of the dependency
 	@Mock
 	private OrderDAO dao;
-	
+
 	@Before
 	public void setup() {
 		MockitoAnnotations.initMocks(this);
+		bo = new OrderBOImpl();
+		bo.setDao(dao);
 	}
 	
 	@Test
 	public void placeOrderShouldCreateAnOrder() throws SQLException, BOException {
-		OrderBOImpl bo = new OrderBOImpl();
-		bo.setDao(dao);
 		Order order = new Order();
 		when(dao.create(order)).thenReturn(new Integer(1));
-		
 		boolean result = bo.placeOrder(order);
 		
-		// Verify bo "placeOrder()" returned "true"
 		assertTrue(result);
-		
-		// Verify bo actually called mock "create()"
 		Mockito.verify(dao).create(order);
 	}
 
+	@Test
+	public void placeOrderShouldNotCreateAnOrder() throws SQLException, BOException {
+		Order order = new Order();
+		when(dao.create(order)).thenReturn(new Integer(0));		
+		boolean result = bo.placeOrder(order);
+
+		assertFalse(result);
+		verify(dao).create(order);
+	}
+
+	@Test(expected=BOException.class)  // JUnit4 syntax
+	public void placeOrderShouldThrowBOException() throws SQLException, BOException {
+		Order order = new Order();
+		when(dao.create(order)).thenThrow(SQLException.class);		
+		boolean result = bo.placeOrder(order);
+	}
+
+	@Test
+	public void cancelOrderShouldCancelTheOrder() throws SQLException, BOException {
+		// Set expections
+		Order order = new Order();
+		when(dao.read(ID)).thenReturn(order);	
+		when(dao.update(order)).thenReturn(1);	
+		
+		boolean result = bo.cancelOrder(ID);
+		
+		// Verify results
+		assertTrue(result);
+		verify(dao).read(ID);
+		verify(dao).update(order);
+	}
+
+	@Test
+	public void cancelOrderShouldNotCancelTheOrder() throws SQLException, BOException {
+		// Set expections
+		Order order = new Order();
+		when(dao.read(ID)).thenReturn(order);	
+		when(dao.update(order)).thenReturn(0);	
+		
+		boolean result = bo.cancelOrder(ID);
+		
+		// Verify results
+		assertFalse(result);
+		verify(dao).read(ID);
+		verify(dao).update(order);
+	}
+
+	@Test(expected=BOException.class)  // JUnit4 syntax
+	public void cancelOrderShouldThrowBOExceptionOnRead() throws SQLException, BOException {
+		// Set ONE expection, DELETE everything else
+		when(dao.read(ID)).thenThrow(SQLException.class);	
+		boolean result = bo.cancelOrder(ID);
+	}
+
+	@Test(expected=BOException.class)  // JUnit4 syntax
+	public void cancelOrderShouldThrowBOExceptionOnUpdate() throws SQLException, BOException {
+		Order order = new Order();
+		when(dao.read(ID)).thenReturn(order);	// We need to do this...
+		when(dao.update(order)).thenThrow(SQLException.class);	// ... before we can test this
+		boolean result = bo.cancelOrder(ID);
+	}
+	
+	// "deleteOrder()" unit tests TBD... 
 }
